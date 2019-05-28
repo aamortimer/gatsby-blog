04a5061c525febd8f5ede3ecb9f26971d7903f78
Andy Mortimer <andy.mortimer@specialistholidays.com>
2019-05-28 20:38:35 +0100

    Remove feeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
   },
   plugins: [
-		`gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -54,7 +54,6 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
